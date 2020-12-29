2871f29cb359720482bc65684c3add1836ce3069
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-29 22:19:43 +0900

    anonymous function on click event test

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
-  return(
+  
+  const handleClick = (event) => {
+    console.log("Hi everyone!");
+  }
+
+  const handleClickAgain = (name, event) => {
+    console.log(`Hi ${name}!`, event.target);
+  }
+  
+  return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me!</button>
+      {/* Cannot use paraenthesis otherwise the function is automatically invoked without a click. So use an anoymous function */}
+      <button onClick={(event) => handleClickAgain("Tomo", event)}>Click Me Again!</button>
     </div>
    );
 }
