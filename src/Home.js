6661cbce5d49704121e81f13b12372dd4a1fd8ca
Tomo Norman <tomo.l.norman@gmail.com>
2021-01-09 13:03:38 +0900

    json db added

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "Welcome to my New Blog!", body: "lorem ipsum...", author: "Tomo", id: 1 },
-    { title: "Job Hunting after finishing a bootcamp", body: "lorem ipsum...", author: "Taku", id: 2 },
-    { title: "Birthday Party for a 2 year old", body: "lorem ipsum...", author: "Tomo", id: 3 }
-  ]);
-
-  const [name, setName] = useState("Tomo");
-  
-  const handleDelete = (id) => {
-    // Don't mutate original staste, but create new state by filtering:
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    // setBlogs to be the new array without deleted item
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Hi Tomo");
-  }, [name]); // The second argument prevents useEffect from firing everytime there is a rerender, only what changes inside the array
+    fetch("http://localhost:8000/blogs")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      });
+  }, []); // The second argument prevents useEffect from firing everytime there is a rerender, only what changes inside the array
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("Keiko")}>Change Name</button>
-      <p>{ name }</p>
+      { blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
    );
 }
