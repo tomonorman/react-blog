d1b5ac3979a5d3cb3681c90b4bce5c8778f09691
Tomo Norman <tomo.l.norman@gmail.com>
2021-01-20 10:55:01 +0900

    create blog form added

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("tomo");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <label>Blog Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
+        <label>Blog Author</label>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="tomo">Tomo</option>
+          <option value="taku">Taku</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 }
